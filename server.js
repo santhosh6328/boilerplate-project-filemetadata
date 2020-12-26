b2a585509c2fcad2b15555b476bef867b93aba34
santhosh <santhosh6328@gmail.com>
2020-12-27 00:23:24 +0530

    modify json response

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/', function (req, res) {
 
 app.post("/api/fileanalyse", upload.single('upfile'), (req, res) => {
   result = {
-    filename: req.file.originalname,
+    name: req.file.originalname,
     type: req.file.mimetype,
     size: req.file.size
   }
